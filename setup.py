2f890c8154c4c08014b4d698826f401d49331d33
kfarjam90 <145766708+kfarjam90@users.noreply.github.com>
2025-06-22 18:04:53 +0200

    data ingestion complited

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Chicken-Disease-Classification"
-AUTHOR_USER_NAME = "entbappy"
+AUTHOR_USER_NAME = "kfarjam90"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "kfarKFARJAM@ibs-b.hu"
 
 
 setuptools.setup(
